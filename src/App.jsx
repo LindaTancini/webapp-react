4d543055148cab738a5d816dabe8eb89c8f04b4e
LindaTancini <lindatancini95@gmail.com>
2025-05-12 15:51:07 +0200

    feat: add axios

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 //Importazioni
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import HomePage from "./pages/Homepage";
-import Movies from "./pages/Movies";
+import MoviesPage from "./pages/Movies";
 import MovieDetails from "./pages/MovieDetails";
 import DefaultLayout from "./layouts/DefaultLayout";
 
@@ -12,7 +12,7 @@ function App() {
         <Routes>
           <Route element={<DefaultLayout />}>
             <Route path="/" element={<HomePage />} />
-            <Route path="/movies" element={<Movies />} />
+            <Route path="/movies" element={<MoviesPage />} />
             <Route path="/movies/:id" element={<MovieDetails />} />
           </Route>
         </Routes>
--- src/pages/Movies.jsx
+++ src/pages/Movies.jsx
@@ -1,5 +1,41 @@
-function Movies() {
-  return <div>Qua si vedranno i film</div>;
+//Importazioni
+import { useState } from "react";
+import MovieCard from "../components/MovieCard";
+import axios from "axios";
+
+function MoviesPage() {
+  const [movies, setMovies] = useState([]);
+
+  //Axios
+  function getMovies() {
+    const url = "http://localhost:3000/api/movies";
+    axios
+      .get(url)
+      .then((res) => console.log(res))
+      .catch((err) => console.log(err));
+  }
+
+  getMovies();
+
+  return (
+    <>
+      <h1>Film</h1>
+      <section>
+        <h2>Lista dei film:</h2>
+        <div className="flex flex-wrap gap-4 justify-center">
+          {movies.length ? (
+            movies.map((movie) => (
+              <div>
+                <MovieCard />
+              </div>
+            ))
+          ) : (
+            <div>Nessun Film trovato</div>
+          )}
+        </div>
+      </section>
+    </>
+  );
 }
 
-export default Movies;
+export default MoviesPage;
