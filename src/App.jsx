89b31da28eed02eccdb7baa7cb6c903b41b52868
LindaTancini <lindatancini95@gmail.com>
2025-05-12 14:56:13 +0200

    feat: create layout

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,18 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import HomePage from "./pages/Homepage";
 import Movies from "./pages/Movies";
 import MovieDetails from "./pages/MovieDetails";
+import DefaultLayout from "./layouts/DefaultLayout";
 
 function App() {
   return (
     <>
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/movies" element={<Movies />} />
-          <Route path="/movies/:id" element={<MovieDetails />} />
+          <Route element={<DefaultLayout />}>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/movies" element={<Movies />} />
+            <Route path="/movies/:id" element={<MovieDetails />} />
+          </Route>
         </Routes>
       </BrowserRouter>
     </>
