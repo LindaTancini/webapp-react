765d0314dae8a785d26372adfdfe49bbe4aeea8d
LindaTancini <lindatancini95@gmail.com>
2025-05-12 17:25:48 +0200

    feat: add search and some style

--- src/pages/Movies.jsx
+++ src/pages/Movies.jsx
@@ -5,12 +5,15 @@ import axios from "axios";
 
 function MoviesPage() {
   const [movies, setMovies] = useState([]);
+  const [search, setSearch] = useState("");
 
   //Axios
   function getMovies() {
     const url = "http://localhost:3000/api/movies";
     axios
-      .get(url)
+      .get(url, {
+        params: { search },
+      })
       .then((res) => {
         console.log(res.data);
         setMovies(res.data);
@@ -18,13 +21,44 @@ function MoviesPage() {
       .catch((err) => console.log(err));
   }
 
+  //Ricerca di un film
+  function searchMovies(event) {
+    event.preventDefault();
+
+    getMovies();
+  }
+
   useEffect(getMovies, []);
 
   return (
     <>
-      <h1>Film</h1>
-      <section>
-        <h2>Lista dei film:</h2>
+      <h1 className="text-4xl font-semibold text-center text-gray-800 mb-6">
+        Film
+      </h1>
+      <section className="p-6">
+        <h2 className="text-2xl font-bold text-gray-700 mb-4">
+          Lista dei film:
+        </h2>
+
+        <form
+          onSubmit={searchMovies}
+          className="flex justify-end items-center space-x-2  mb-6"
+        >
+          <input
+            type="text"
+            placeholder="Cerca un film"
+            value={search}
+            onChange={(e) => setSearch(e.target.value)}
+            className="px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-400 w-64"
+          />
+          <button
+            type="submit"
+            className="bg-blue-500 text-white font-bold py-2 px-4 rounded-full focus:outline-none focus:ring-2 focus:ring-blue-400"
+          >
+            Cerca
+          </button>
+        </form>
+
         <div className="flex flex-wrap gap-4 justify-center">
           {movies.length ? (
             movies.map((movie) => <MovieCard key={movie.id} data={movie} />)
