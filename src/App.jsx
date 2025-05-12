7bb1ed870c917afca15f988888018d8acac569a0
LindaTancini <lindatancini95@gmail.com>
2025-05-12 14:15:40 +0200

    feat: add routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,16 @@
+//Importazioni
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
 function App() {
   return (
     <>
-      <h1>Benvenuti nella mia webapp</h1>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<div>HomePage</div>} />
+          <Route path="/movies" element={<div>Elenco dei film</div>} />
+          <Route path="/movies/:id" element={<div>Dettaglio del film</div>} />
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
