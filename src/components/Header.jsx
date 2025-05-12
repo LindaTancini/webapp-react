a9d92dde15493b3deda86da13393c110c2061875
LindaTancini <lindatancini95@gmail.com>
2025-05-12 14:43:40 +0200

    feat: create header

---
+++ src/components/Header.jsx
@@ -0,0 +1,20 @@
+//Importazioni
+import { NavLink } from "react-router-dom";
+
+function Header() {
+  return (
+    <nav>
+      <div>Logo</div>
+      <ul>
+        <li>
+          <NavLink to="/">HomePage</NavLink>
+        </li>
+        <li>
+          <NavLink to="/movies">Movies</NavLink>
+        </li>
+      </ul>
+    </nav>
+  );
+}
+
+export default Header;
